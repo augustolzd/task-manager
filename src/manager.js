10758baeb91b6b950c08fc46fbe1270e15b83724
Augusto Lozada <Augustolzd@gmail.com>
2021-08-27 12:36:35 -0500

    Adding time to run each interation

--- src/manager.js
+++ src/manager.js
@@ -4,20 +4,19 @@ const [, , taskFile, balancer] = argv;
 const task = require(taskFile);
 
 async function handleTask() {
-  if (task.interval) {
-    setInterval(() => {
-      task.task(balancer);
-    }, task.interval);
-  } else {
-    await task.task(balancer);
-    handleTask();
-  }
+  await task.task(balancer);
+
+  await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, task.interval * 1000 || 1000);
+  });
 }
+
 async function initTask() {
   if (task.init && typeof task.init === 'function') {
     await task.init();
   }
-
   handleTask();
 }
 
